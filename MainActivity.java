6c86964461f95f9d53f140bad406d4c650222f4e
dxtg <2549744283@qq.com>
2018-01-23 18:32:03 +0800

    第一次提交的项目

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package bawei.com.day01_gitgub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
